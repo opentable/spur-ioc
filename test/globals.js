ecb97c5a44c7a17645fdc9b5ee814316eae31579
Agustin Colchado <agustin@colchado.com>
2016-06-12 14:29:18 -0700

    Updating test code to be valid based on the linter settings.

--- test/globals.js
+++ test/globals.js
@@ -1,6 +1,6 @@
-global.assert = require("assert")
-global.chai = require("chai")
-global.expect = global.chai.expect
-process.env.NODE_ENV = "test"
+global.assert = require('assert');
+global.chai = require('chai');
+global.expect = global.chai.expect;
+process.env.NODE_ENV = 'test';
 
-process.setMaxListeners(1000)
+process.setMaxListeners(1000);
